43886c9cd31667b6fb3c7fe4d784bec9c6c4e6d0
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-23 18:20:54 +0200

    add preliminary endpoints

---
+++ api/src/app.ts
@@ -0,0 +1,31 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import fs from 'fs';
+import path from 'path';
+import { Parser } from './services/Parser';
+import { Enricher } from './services/Enricher';
+
+const app = new Koa();
+const router = new Router();
+const parser = new Parser();
+const enricher = new Enricher();
+const statusFile = fs.readFileSync(path.join(__dirname, 'status.real.txt')).toString();
+const packageObjects = enricher.enrichPackageObjects(parser.parseStatusToObjects(statusFile));
+
+router.get('/api/packages', ctx => {
+    ctx.body = packageObjects;
+});
+
+router.get('/api/packages/names', ctx => {
+  ctx.body = packageObjects.map(pkg => pkg.packageName);
+});
+
+router.get('/api/packages/:packageName', ctx => {
+    const result = packageObjects.filter(pkg => pkg.packageName === ctx.params.packageName);
+    if (result.length === 0) return (ctx.status = 404);
+    return (ctx.body = result);
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000);
