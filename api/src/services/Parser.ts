4d2858ab6e2b72443469cb00d9a9a546a913185a
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-23 13:32:08 +0200

    implement parser

---
+++ api/src/services/Parser.ts
@@ -0,0 +1,41 @@
+export interface PackageShape {
+    package: string;
+    description: string;
+    depends: string[];
+}
+
+export class Parser {
+    public parseStatusToObjects = (status: string): PackageShape[] => {
+        const packages = status.trim().split(/\n{2,}/g); // Split each package
+        const keysToParse = ['package', 'description', 'depends'];
+        return packages.map((pkg: string) => {
+            return pkg
+                .split(/\n(?!\s)/) // Split fields e.g ['Package: package-name', 'Depends: dependency-name']
+                .map(line => line.split(/:([\s\S]+)/)) // Split key and value e.g ['Package', 'package-name']
+                .reduce((packageObject: PackageShape, splitLine) => {
+                    const key = splitLine[0].toLowerCase() as keyof PackageShape;
+                    if (keysToParse.includes(key)) {
+                        if (key === 'depends') {
+                            packageObject[key] =
+                                splitLine[1]
+                                    // Use capture group to preserve delimiters
+                                    // so they can be used to categorize dependencies
+                                    .split(/([,|])/g)
+                                    .map(dependency => dependency && dependency.trim()) || [];
+                        } else if (key === 'description') {
+                            packageObject[key] = splitLine[1]
+                                .trim()
+                                .replace(/(\s\.)/g, '')
+                                .replace(/\n\s/g, '\n');
+                        } else {
+                            packageObject[key] = splitLine[1].trim();
+                        }
+                    }
+                    if (!packageObject['depends']) {
+                        packageObject['depends'] = [];
+                    }
+                    return packageObject;
+                }, {} as PackageShape);
+        });
+    };
+}
