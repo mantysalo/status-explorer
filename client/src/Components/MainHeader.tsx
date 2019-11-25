7c42f8078177eb4e14140a1eb45f56ff5fd2c724
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-25 11:56:28 +0200

    remove side margins

--- client/src/Components/MainHeader.tsx
+++ client/src/Components/MainHeader.tsx
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const MainHeader = styled.h1`
     font-size: 2em;
     font-weight: 600;
-    margin: 8px;
+    margin: 8px 0px;
     font-family: Arial, Helvetica, sans-serif;
 `;
