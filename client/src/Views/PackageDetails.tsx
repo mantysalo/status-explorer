15466a05bb30847487239da982d3324d2fb8e4ca
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-26 15:19:38 +0200

    format description in the frontend

--- client/src/Views/PackageDetails.tsx
+++ client/src/Views/PackageDetails.tsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { MainHeader } from '../Components/MainHeader';
 import { EnrichedPackageShape } from '../../../api/src/services/Enricher';
-import {API_URL} from '../config'
+import { API_URL } from '../config';
 import styled from 'styled-components';
+import { FlexCenter } from '../Components/FlexCenter';
 
 type PackageDetailsProps = {
     packageNames: string[];
@@ -14,7 +15,6 @@ type SeparatedListItemProps = {
 };
 
 const SeparatedList = styled.ul`
-    display: inline;
     list-style: none;
 
     li:last-child:after {
@@ -29,7 +29,7 @@ const SeparatedListItem = styled.li`
     }
 `;
 
-const ShortDescription = styled.p`
+const ShortDescription = styled.strong`
     font-weight: 600;
 `;
 
@@ -39,65 +39,97 @@ const LongDescription = styled.p`
 
 const SubHeader = styled.h2`
     font-weight: 600;
-    font-size: 1.5em;
+    font-size: 1.5rem;
     margin: 8px 0px;
     font-family: Arial, Helvetica, sans-serif;
 `;
 
+// CRA does not allow relative imports outside of src so this is copied from
+// api/src/services/Enricher.ts
+const stripVersionNumber = (dependency: string): string => {
+    return dependency.replace(/ *\([^)]*\) */g, '');
+};
+
 export const PackageDetails = ({ packageNames }: PackageDetailsProps) => {
     const { packageName } = useParams();
     const [pkg, setPkg] = useState<EnrichedPackageShape>();
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const fetchData = async () => {
-            const packageData: EnrichedPackageShape = await (await fetch(`${API_URL}/api/packages/${packageName}`)).json();
+            const packageData: EnrichedPackageShape = await (
+                await fetch(`${API_URL}/api/packages/${packageName}`)
+            ).json();
             setPkg(packageData);
+            setLoading(false)
         };
         fetchData();
+        document.title = packageName ? `Package Browser | ${packageName}` : 'Package Browser';
     }, [packageName]);
 
     const isDependedOn = Boolean(pkg && pkg.dependedOnBy.length);
-    const hasDependencies = Boolean(pkg && !!pkg.dependsOn.length);
+    const hasDependencies = Boolean(pkg && pkg.dependsOn.length);
+    if (loading) return <h1>Loading</h1>
     return (
         <div>
-          <nav><Link to="/">Back to listing</Link></nav>
-          {pkg ? ( <>
-                    <header>
-                        <MainHeader>{pkg.packageName}</MainHeader>
-                    </header>
-                    <main>
-                    <ShortDescription>{pkg.shortDescription}</ShortDescription>
-                    <LongDescription>{pkg.longDescription}</LongDescription>
-                    {hasDependencies && (
-                        <>
-                            <SubHeader>Dependencies</SubHeader>
-                            <SeparatedList>
-                                {pkg.dependsOn.map(dep =>
-                                    packageNames.includes(dep.name) ? (
-                                        <SeparatedListItem type={dep.type}>
-                                            <Link to={`/packages/${dep.name}`}>{dep.name}</Link>
-                                        </SeparatedListItem>
-                                    ) : (
-                                        <SeparatedListItem type={dep.type}>{dep.name}</SeparatedListItem>
-                                    )
-                                )}
-                            </SeparatedList>
-                        </>
-                    )}
+            <nav>
+                <Link to='/'>Back to listing</Link>
+            </nav>
+            {pkg &&
+                <>
+                    <FlexCenter>
+                        <header>
+                            <MainHeader>{pkg.packageName}</MainHeader>
+                        </header>
 
-                    {isDependedOn && (
-                        <>
-                            <SubHeader>Depended on by</SubHeader>
-                            <ul>
-                                {pkg.dependedOnBy.map((dep: string) => (
-                                    <li>
-                                        <Link to={`/packages/${dep}`}>{dep}</Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </>
-                    )}
-                    </main>
-          </>): <h1>Loading</h1>}
-        </div> 
+                        <main>
+                            <SubHeader>Description</SubHeader>
+                            <ShortDescription>{pkg.shortDescription}</ShortDescription>
+                            <LongDescription>
+                                {pkg.longDescription
+                                    .replace(/  +/g, ' ') // Turn 2 or more spaces in to one
+                                    .replace(/\n (?![-*])/g, '') // Remove linebreaks from non-list items
+                                    .replace(/\s\.\s\n|\s\.\s/g, '\n\n') // Replace paragraph separators with line breaks
+                                }
+                            </LongDescription>
+                            {hasDependencies && (
+                                <>
+                                    <SubHeader>Dependencies</SubHeader>
+                                    <SeparatedList>
+                                        {pkg.dependsOn.map(dep =>
+                                            packageNames.includes(stripVersionNumber(dep.name)) ? (
+                                                <SeparatedListItem key={dep.name} type={dep.type}>
+                                                    <Link
+                                                        key={dep.name}
+                                                        to={`/packages/${stripVersionNumber(dep.name)}`}
+                                                    >
+                                                        {dep.name}
+                                                    </Link>
+                                                </SeparatedListItem>
+                                            ) : (
+                                                <SeparatedListItem key={dep.name} type={dep.type}>
+                                                    {dep.name}
+                                                </SeparatedListItem>
+                                            )
+                                        )}
+                                    </SeparatedList>
+                                </>
+                            )}
+
+                            {isDependedOn && (
+                                <>
+                                    <SubHeader>Depended on by</SubHeader>
+                                    <SeparatedList>
+                                        {pkg.dependedOnBy.map((dep: string) => (
+                                            <SeparatedListItem type='normal'>
+                                                <Link to={`/packages/${dep}`}>{dep}</Link>
+                                            </SeparatedListItem>
+                                        ))}
+                                    </SeparatedList>
+                                </>
+                            )}
+                        </main>
+                    </FlexCenter>
+                </>}
+        </div>
     );
 };
