e99bbb9447233625e5aa3b85a783d27d15658fb9
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-26 14:56:05 +0200

    add padding and link styling globally

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Reset } from 'styled-reset'
+import { Reset } from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: Helvetica, Verdana, sans-serif;;
+    font-family: Helvetica, Verdana, sans-serif;
+    color: #001F3F;
+    padding: 8px;
+    a:link {
+        color: #0074D9;
+    }
+    a:visited {
+        color: #B10DC9;
+    }
   }
-`
+`;
 
-ReactDOM.render(<><Reset/><App /><GlobalStyle/></>, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <App />
+        <Reset />
+        <GlobalStyle />
+    </>,
+    document.getElementById('root')
+);
