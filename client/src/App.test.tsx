715015e24f8d0a993bca31f989e5c55305764cfb
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-26 18:09:35 +0200

    create tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, cleanup, waitForElement, wait, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { EnrichedPackageShape } from '../../api/src/services/Enricher';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App/>', () => {
+    afterEach(() => {
+        cleanup();
+    });
+    it('renders', async () => {
+        jest.spyOn(window, 'fetch').mockImplementationOnce(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve({ data: ['foo', 'bar', 'baz'] }),
+            } as Response);
+        });
+        const { getByText } = render(<App />);
+        expect(getByText('Packages')).toBeInTheDocument();
+        // Wait for the useEffect to run, to avoid warnings in terminal.
+        await wait(() => expect(getByText('foo')).toBeInTheDocument());
+    });
+
+    it('allows navigating to a package', async () => {
+        const packageObject: EnrichedPackageShape = {
+            packageName: 'foo',
+            dependsOn: [{ name: 'bar', type: 'normal' }],
+            dependedOnBy: ['baz'],
+            shortDescription: 'foo package',
+            longDescription: 'Long description',
+        };
+        jest.spyOn(window, 'fetch')
+            .mockImplementationOnce(() => {
+                return Promise.resolve({
+                    json: () => Promise.resolve({ data: ['foo', 'bar', 'baz'] }),
+                } as Response);
+            })
+            .mockImplementationOnce(() => {
+                return Promise.resolve({
+                    json: () => Promise.resolve(packageObject),
+                } as Response);
+            });
+        const { getByText } = render(<App />);
+        const fooListItem = await waitForElement(() => getByText('foo'));
+        fireEvent.click(fooListItem);
+        
+        await waitForElement(() => getByText(packageObject.packageName));
+        await waitForElement(() => getByText(packageObject.dependsOn[0].name));
+        await waitForElement(() => getByText(packageObject.dependedOnBy[0]));
+        await waitForElement(() => getByText(packageObject.shortDescription));
+        await waitForElement(() => getByText(packageObject.longDescription));
+    });
 });
---
+++ client/src/Views/FrontPage/FrontPage.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from '../../App';
+import { MemoryRouter } from 'react-router-dom';
+import { FrontPage } from './FrontPage';
+
+describe('<FrontPage />', () => {
+    it('should render without crashing', () => {
+        render(<FrontPage packageNames={['foo', 'bar', 'baz']} />, { wrapper: MemoryRouter });
+    });
+    it('should render a list with all the provided packages in alphabetical order', async () => {
+        jest.spyOn(window, 'fetch').mockImplementationOnce(() => {
+            return Promise.resolve({
+                json: () => Promise.resolve({ data: ['foo', 'bar', 'baz'] }),
+            } as Response);
+        });
+        const { getAllByRole } = render(<App />);
+       await waitForElement(() => expect(getAllByRole('listitem').length).toBe(3));
+        
+    });
+});
