be625bc7f002e0471302f0b0706f680d6e44f4b7
Riku Mäntysalo <riku.mantysalo@gmail.com>
2019-11-30 14:59:19 +0200

    Updates

---
+++ precache-manifest.5cfca392f6ab832635f631cdbbd90ec4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c326a9fa48197b3cd35cc275ab70b3f",
+    "url": "/status-explorer/index.html"
+  },
+  {
+    "revision": "8ecffa7c87ca225a0483",
+    "url": "/status-explorer/static/js/2.15aac60a.chunk.js"
+  },
+  {
+    "revision": "1495aa91908da635478c",
+    "url": "/status-explorer/static/js/main.221e4760.chunk.js"
+  },
+  {
+    "revision": "60f3ec89c2bb702d7d17",
+    "url": "/status-explorer/static/js/runtime-main.2e517d60.js"
+  }
+]);
