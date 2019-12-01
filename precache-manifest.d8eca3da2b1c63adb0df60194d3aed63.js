39752e982720cf64a3e671031b66343cee501c17
RikuMantysalo <33690850+RikuMantysalo@users.noreply.github.com>
2019-12-01 01:02:23 +0000

    Deploying to gh-pages from master ad91974fa30ec1a8731ac6247a3e4eef98282fa1

---
+++ precache-manifest.d8eca3da2b1c63adb0df60194d3aed63.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232917a16fe005b80fa5b089af2ab595",
+    "url": "/status-explorer/index.html"
+  },
+  {
+    "revision": "3e6cce09beadd68e8ced",
+    "url": "/status-explorer/static/js/2.024ce25f.chunk.js"
+  },
+  {
+    "revision": "def7a985028409218e20",
+    "url": "/status-explorer/static/js/main.878178ee.chunk.js"
+  },
+  {
+    "revision": "60f3ec89c2bb702d7d17",
+    "url": "/status-explorer/static/js/runtime-main.2e517d60.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e793f1cde3f4248b0f97d5f7258107f2.js"
+  "/status-explorer/precache-manifest.d8eca3da2b1c63adb0df60194d3aed63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/status-explorer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
